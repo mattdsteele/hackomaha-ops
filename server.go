3b4ff586fc4d77eb72f00c780d7dbdc76bd3cd35
Matt Steele <orphum@gmail.com>
2013-11-24 06:24:51 -0600

    tabs2spaces

--- server.go
+++ server.go
@@ -16,63 +16,63 @@ func main() {
   db, err := gorm.Open("mysql", "oops:" + dbPassword + "@tcp(15.126.247.23:3306)/oops")
   if err != nil { panic(err) }
   //defer db.Close() //TODO what should this be?
-  
+
   //db.SetPool(10)
-  
+
   db.LogMode(true)
 
   m.Map(db)
-  
+
   m.Get("/schools", func(res http.ResponseWriter) string {
     var allSchools = []School{}
-  	db.Find(&allSchools)
+    db.Find(&allSchools)
     return render(res, allSchools)
   })
 
-//TODO need to also return stats by year -- needs to return []DistrictsByYear - district level stats for each year and each district
+  //TODO need to also return stats by year -- needs to return []DistrictsByYear - district level stats for each year and each district
   m.Get("/districts", func(res http.ResponseWriter) string {
     var allDistricts = []District{}
     db.Find(&allDistricts)
     return render(res, allDistricts)
   })
 
-//TODO need to also return stats by year for each school in the district - needs to return SchoolsByYear[] - data for all schools in this district
+  //TODO need to also return stats by year for each school in the district - needs to return SchoolsByYear[] - data for all schools in this district
   m.Get("/district/:id", func(res http.ResponseWriter, params martini.Params) string {
     var testDistrict = District{}
-  	db.Where("id = ?", params["id"]).First(&testDistrict)
+    db.Where("id = ?", params["id"]).First(&testDistrict)
     return render(res, testDistrict)
 
   })
 
-//TODO Possible clean-up opportunities here
-//TODO Still need total teacher and student count per year - student count could be calculated from grade counts
+  //TODO Possible clean-up opportunities here
+  //TODO Still need total teacher and student count per year - student count could be calculated from grade counts
   m.Get("/school/:id", func(res http.ResponseWriter, params martini.Params) string {
     var testSchool = School{}
-  	db.Where("id = ?",params["id"]).First(&testSchool)
-  	var testStats = []ClassStat{}
-  	db.Where("school_id = ?", params["id"]).Find(&testStats)
-  	
+    db.Where("id = ?",params["id"]).First(&testSchool)
+    var testStats = []ClassStat{}
+    db.Where("school_id = ?", params["id"]).Find(&testStats)
+
     var yearsToEnrollments = map[string][]GradeEnrollment{}
-  	for _, row := range testStats {
-  		yearsToEnrollments[row.Years] = append(yearsToEnrollments[row.Years], GradeEnrollment{
-  			Grade: row.Grade,
-  			EnrollmentSize:    row.EnrollmentSize,
-  		})
-  	}
-  	
-  	var enrollmentData = []EnrollmentByYear{}
-  	for year, enrollment := range yearsToEnrollments{
-  		enrollmentData = append(enrollmentData, EnrollmentByYear{
-  			Year: year,
-  			GradeEnrollment: enrollment,
-  		})
-  	}
-  	
-  	var allData = SchoolWithEnrollment {
-  		School: testSchool,
-  		EnrollmentByYear: enrollmentData,
-  	}
-    return render(res, allData)
+    for _, row := range testStats {
+      yearsToEnrollments[row.Years] = append(yearsToEnrollments[row.Years], GradeEnrollment{
+        Grade: row.Grade,
+        EnrollmentSize:    row.EnrollmentSize,
+      })
+    }
+
+    var enrollmentData = []EnrollmentByYear{}
+    for year, enrollment := range yearsToEnrollments{
+      enrollmentData = append(enrollmentData, EnrollmentByYear{
+        Year: year,
+        GradeEnrollment: enrollment,
+      })
+    }
+
+    var allData = SchoolWithEnrollment {
+      School: testSchool,
+      EnrollmentByYear: enrollmentData,
+    }
+  return render(res, allData)
   })
 
   m.Run()
