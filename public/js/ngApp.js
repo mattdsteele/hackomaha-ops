fb7c89a9ce58b37afdf041e5afe72b28a1f1c3fc
Matt Steele <orphum@gmail.com>
2013-11-24 17:41:24 -0600

    switching to the last value

--- public/js/ngApp.js
+++ public/js/ngApp.js
@@ -29,7 +29,7 @@ opsAppModule.controller('OpsCtrl', function ($scope, OpsApi) {
     OpsApi.getSchool($scope.schoolInView).success(function (data){
       $scope.cardName = data.School.Name;
       var schoolId = data.School.Id;
-      var enrollmentLevel = data.EnrollmentByYear[0];
+      var enrollmentLevel = data.EnrollmentByYear[data.EnrollmentByYear.length - 1];
       $scope.totalEnrollment = enrollmentLevel.Students;
       $scope.totalTeachers = enrollmentLevel.Teachers;
       $scope.drawStackedBar(schoolId);
@@ -49,8 +49,9 @@ opsAppModule.controller('OpsCtrl', function ($scope, OpsApi) {
   }
 
 	 	$scope.yearAry=['2002/2003','2003/2004','2004/2005','2005/2006','2006/2007','2007/2008','2008/2009','2009/2010',
-	 	'2010/2011','2011/2012','2012/2013']
+	 	'2010/2011','2011/2012','2012/2013'];
 
+    $scope.schoolInView = 280001005;
 	 	$scope.schoolId = 280001005; //fixme get rid of this
 	 	//this is the one to use for UI
 	 		 $scope.drawStackedBar = function(schoolId){
