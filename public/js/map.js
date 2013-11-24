642ee7f821413f9a23cf9a21f009a693dd8f24ec
Christopher Kollars <chris@christopherkollars.com>
2013-11-24 10:58:13 -0600

    refactor map js

--- public/js/map.js
+++ public/js/map.js
@@ -1,85 +1,148 @@
-var timeline = [];
+var api_url = "/";
 
-$.getJSON("http://localhost:3000/districts", function(data) {
-  timeline = data;
-});
+var OPS = {
+  curData: [],
 
-MQA.EventUtil.observe(window, 'load', function() {
+  init: function(){
 
-  /*Create an object for options*/
-  var options={
-    elt:document.getElementById('map'),
-    zoom:10,
-    latLng:{lat:41.25, lng:-96},
-    mtype:'map',
-    bestFitMargin:0,
-    zoomOnDoubleClick:false
-  };
+    // $.getJSON("http://15.126.247.23:3000/districts", function(json) {
+    //   OPS.data = json;
+    // });
+    maps.init();
+  }
 
-  /*Construct an instance of MQA.TileMap with the options object*/
-  window.map = new MQA.TileMap(options);
+};
 
-// MQA.EventManager.addListener(map, 'shapecollectionadded', loadData);
-//  MQA.EventManager.addListener(map, 'movestart', loadData);
-//  MQA.EventManager.addListener(map, 'move', loadData);
-//  MQA.EventManager.addListener(map, 'moveend', loadData);
-//  MQA.EventManager.addListener(map, 'dragstart', loadData);
-//  MQA.EventManager.addListener(map, 'drag', loadData);
-//  MQA.EventManager.addListener(map, 'dragend', loadData);
-//  MQA.EventManager.addListener(map, 'zoomstart', loadData);
-//  loadDataMQA.EventManager.addListener(map, 'zoomend', loadData);
-  var schoolsShapeCollection = new MQA.ShapeCollection();
-	var createShapeCollection = function(){
-    map.addControl(
-      new MQA.SmallZoom(),
-      new MQA.MapCornerPlacement(MQA.MapCorner.TOP_LEFT, new MQA.Size(5,5))
-    );
+var maps = {
 
-    var disctricts = new MQA.ShapeCollection();
-    disctricts.collectionName = 'nebraska_disctricts';
-    disctricts.maxZoomLevel = 7;
+  init: function(){
+    console.log('init');
+    MQA.EventUtil.observe(window, 'load', function() {
+      var options={
+        elt:document.getElementById('map'),
+        zoom:10,
+        latLng:{lat:41.25, lng:-96},
+        mtype:'map',
+        bestFitMargin:0,
+        zoomOnDoubleClick:true
+      };
 
-    for(i=0; i < timeline.length; i++){
-      var disctrictCircle = new MQA.CircleOverlay();
+      window.map = new MQA.TileMap(options);
 
-      var district = timeline[i]['Districts'][0];
-      var districtSize = timeline[i]['Districts'][0]['EnrollmentSize'] * 0.05;
-      var districtID = timeline[i]['Districts'][0]['District']['Id'];
-      var latLong = timeline[i]['Districts'][0]['District']['Latitude'] + ', ' + timeline[i]['Districts'][0]['District']['Longitude'];
+      MQA.withModule('smallzoom','shapes', function() {
 
-      disctrictCircle.radiusUnit = 'MI';
-      disctrictCircle.radius = districtSize;
-      disctrictCircle.shapePoints = [timeline[i]['Districts'][0]['District']['Latitude'], timeline[i]['Districts'][0]['District']['Longitude']];
-      disctrictCircle.colorAlpha = 0.5;
-      disctrictCircle.borderWidth = 0;
-      disctrictCircle.fillColor = '#05c0e2';
-      disctrictCircle.fillColorAlpha = 0.4;
-      disctrictCircle.district_id = districtID;
+        map.addControl(
+          new MQA.SmallZoom(),
+          new MQA.MapCornerPlacement(MQA.MapCorner.TOP_LEFT, new MQA.Size(5,5))
+        );
 
-      MQA.EventManager.addListener(disctrictCircle, 'dblclick', getDistrict);
+        maps.loadCollectionData('districts','');
 
-      disctricts.add(disctrictCircle);
+
+
+      });
+    });
+  },
+
+  loadCollectionData: function(type, id){
+    var newDataURL = api_url + type + id;
+
+    // console.log(newDataURL);
+
+    $.getJSON(newDataURL, function(json) {
+      OPS.curData = json;
+      switch(type){
+        case 'districts':
+          maps.addDistrictCollection(OPS.curData, 0);
+          // console.log('districts loading...');
+          break;
+        case 'schools':
+          maps.addSchoolsCollection(OPS.curData);
+          break;
+      }
+    });
+
+
+  },
+
+  addDistrictCollection: function(data, year){
+    var collection = new MQA.ShapeCollection();
+    collection.maxZoomLevel = 7;
+    var d = data[year]['Districts'];
+
+    // console.log(d);
+    for(i=0; i < d.length; i++){
+      var mapCircle = new MQA.CircleOverlay();
+
+      var district = d[i];
+      var districtSize = district['EnrollmentSize'] * 0.05;
+      var districtID = district['District']['Id'];
+
+      console.log(district['District']['Longitude']);
+
+      mapCircle.radiusUnit = 'MI';
+      mapCircle.radius = districtSize;
+      mapCircle.shapePoints = [district['District']['Latitude'], district['District']['Longitude']];
+      mapCircle.colorAlpha = 0.5;
+      mapCircle.borderWidth = 0;
+      mapCircle.fillColor = '#05c0e2';
+      mapCircle.fillColorAlpha = 0.05;
+      mapCircle.district_id = districtID;
+
+      MQA.EventManager.addListener(mapCircle, 'click', loadCollection);
+
+      collection.add(mapCircle);
     }
 
-    map.addShapeCollection(disctricts);
-
-
-
+    map.addShapeCollection(collection);
     map.bestFit();
-	}	
-  MQA.withModule('smallzoom','shapes', function() {
-		createShapeCollection();
-  });
-	/**
-	when double clicked, this should get all the schools for district to be inserted in the shape Collection and 
-	**/
-	
-  function getDistrict(evt){
-    var district_url = "http://15.126.247.23:3000/district/" + this.district_id;
-    $.getJSON(district_url, function(data) {
-      console.log(data);
-			//schoolsShapeCollection
-			//$scope.addEnrollmentChart
-    });
+
+  },
+
+  addSchoolsCollection: function(data){
+    var collection = new MQA.ShapeCollection();
+    collection.maxZoomLevel = 7;
+
+    for(i=0; i < data.length; i++){
+      var mapCircle = new MQA.CircleOverlay();
+
+      var district = data[i]['Districts'][i];
+      var districtSize = data[i]['Districts'][i]['EnrollmentSize'] * 0.05;
+      var districtID = data[i]['Districts'][i]['District']['Id'];
+      var latLong = data[i]['Districts'][i]['District']['Latitude'] + ', ' + data[i]['Districts'][i]['District']['Longitude'];
+
+      mapCircle.radiusUnit = 'MI';
+      mapCircle.radius = districtSize;
+      mapCircle.shapePoints = [data[i]['Districts'][i]['District']['Latitude'], data[i]['Districts'][i]['District']['Longitude']];
+      mapCircle.colorAlpha = 0.5;
+      mapCircle.borderWidth = 0;
+      mapCircle.fillColor = '#05c0e2';
+      mapCircle.fillColorAlpha = 0.4;
+      mapCircle.district_id = districtID;
+
+      MQA.EventManager.addListener(mapCircle, 'click', getDistrict);
+
+      collection.add(mapCircle);
+    }
+
+    map.addShapeCollection(collection);
+    map.bestFit();
+
   }
-});
+
+};
+
+
+
+
+function loadCollection(evt){
+  var district_url = "http://15.126.247.23:3000/district/" + this.district_id;
+  $.getJSON(district_url, function(data) {
+    console.log(data);
+  });
+}
+(function($){
+
+  OPS.init();
+
+})(jQuery);
