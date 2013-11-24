1b8221950c8617a2b587b4b33f44c8eba2cc8ed0
Matt Steele <orphum@gmail.com>
2013-11-24 00:29:15 -0600

    Split structs out into separate file; udate readme

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ hackomaha-ops
 * Install Go
 * `go get github.com/codegangsta/martini`
 * `go get github.com/go-sql-driver/mysql`
-* `go run server.go`
+* `go get github.com/jinzhu/gorm`
+* `go run *.go`
 * `export OOPS_DB_PASS=TALK_TO_ME`
 * Check http://localhost:3000
--- server.go
+++ server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-  //"database/sql"
-  //_ "github.com/go-sql-driver/mysql"
   "github.com/codegangsta/martini"
   "encoding/json"
   "net/http"
@@ -92,70 +90,3 @@ func asJson(res http.ResponseWriter, data []byte) string {
   return string(data[:])
 }
 
-//Structs
-type School struct {
-  Id    	  int64
-  Name        string `sql:"size:255"`
-  CountyId    int64
-  DistrictId  int64
-  //Latitude    float64
-  //Longitude   float64
-}
-
-type SchoolsByYear struct {
-  Year        string
-  Schools     []SchoolYear
-}
-
-type SchoolYear struct {
-  EnrollmentSize  int64
-  School          School
-}
-
-type District struct {
-  Id              int64
-  Name            string
-  CountyId		  int64
-  //Latitude        float64
-  //Longitude       float64
-}
-
-type SchoolWithEnrollment struct {
-  School            School
-  EnrollmentByYear  []EnrollmentByYear
-}
-
-//Class_Stats table
-//id, schoolId, years, grade, maleStudents, femaleStudents, EnrollmentSize
-
-type ClassStat struct {
-	Id			int64
-	SchoolId	int64
-	Years		string
-	Grade		string
-	MaleStudents	int64
-	FemaleStudents	int64
-	EnrollmentSize	int64
-}
-
-type EnrollmentByYear struct { //EnrollmentByYear
-  Year          string
-  Teachers      int64
-  Students      int64
-  GradeEnrollment []GradeEnrollment
-}
-
-type GradeEnrollment struct {
-  Grade         	string
-  EnrollmentSize    int64
-}
-
-type DistrictsByYear struct {
-  Year      string
-  Districts []DistrictYear
-}
-
-type DistrictYear struct {
-  EnrollmentSize  int64
-  District        District
-}
